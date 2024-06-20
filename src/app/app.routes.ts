04640335a552a69386016f45e50a32fde3c83844
Pedro Hurtado <pedrohurt@gmail.com>
2024-06-20 14:31:25 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'pagina1',
+    loadComponent:()=>import('./pagina1/pagina1.component').then(c=>c.Pagina1Component)
+  },
+
+  {
+    path:'about',
+    loadComponent:()=>import('./about/about.component').then(c=>c.AboutComponent)
+  }
+];
